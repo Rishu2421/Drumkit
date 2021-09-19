b5414eb1c4d09741930b9163e8a28f6d5643f5ae
Rishu2421 <81182028+Rishu2421@users.noreply.github.com>
2021-09-19 15:39:06 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,57 @@
+var numberOfButton = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfButton; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonPressed = this.innerHTML;
+    makeSound(buttonPressed);
+    buttonAnimation(buttonPressed);
+  });
+}
+
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "a":
+      var kickBass = new Audio("sounds/kick-bass.mp3");
+      kickBass.play();
+      break;
+    case "s":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "d":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "j":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "k":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "l":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    default:
+      console.log("Wrong button pressed");
+  }
+}
+
+function buttonAnimation(keyValue){
+    var activeButton= document.querySelector("."+keyValue);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100);
+}
